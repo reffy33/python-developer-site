dfdaea53f9113b764dadb4ffe5537cb0a4bfaebd
reffy33 <reffymin@gmail.com>
2023-02-05 22:39:35 +0500

    join_all added

--- src/main_refactor.py
+++ src/main_refactor.py
@@ -27,34 +27,27 @@ class DemandAnalitic():
         self.vac_count = self.__vac_count__(df)
         self.vac_count_prof = self.__vac_count__(prof_df)
 
+        self.joined = self.join_all()
+
     def __med_salary__(self, df):
         med_salary = get_analitic(df, "median")
-        med_salary.rename(columns={'salary': 'med_salary'})
-
-        return med_salary
+        return med_salary.rename(columns={'salary': 'med_salary'})
 
     def __vac_count__ (self, df):
         vac_count = get_analitic(df, "count")
-        vac_count.rename(columns={"salary": "vac_count"})
-
-        return vac_count
+        return vac_count.rename(columns={"salary": "vac_count"})
 
     def join_all(self):
-        df = self.med_salary.join(self.vac_count)
-        return df
+        return self.med_salary.join(self.vac_count)
     
 
 
-df = pd.read_csv('csv/converted_salary.csv')
+df = pd.read_csv('src/csv/converted_salary.csv')
 df['year'] = df['published_at'].str[:4]
 df = df.drop(columns=['published_at'])
 
 key_words = ["python", "питон", "пайтон"]
 
 demand = DemandAnalitic(df, key_words)
-md = demand.med_salary
-vc = demand.vac_count
 
-# print (md)
-print()
-print(vc)
+print(demand.joined)
