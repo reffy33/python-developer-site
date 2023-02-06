e3dbaa44e463f554cd5bcbbc1f722c74979e4c99
reffy33 <reffymin@gmail.com>
2023-02-06 20:18:52 +0500

    DemandAnalitic moved to separate file

--- src/demand_analitic.py
+++ src/demand_analitic.py
@@ -1,27 +1,9 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import sqlite3
-
-def get_analitic(df, agg_func):
-    return df.groupby("year").agg({"salary": agg_func})
-
-
-def get_profession_df(df, key_words):
-    first = True
-    for word in key_words:
-        if first:
-            first = False
-            mask = df["name"].str.contains(word)
-        else:
-            new_mask = df["name"].str.contains(word)
-            mask = mask | new_mask
-
-    return df.loc[mask]
 
 
 class DemandAnalitic():
     def __init__(self, df, key_words, prefix="python") -> None:
-        prof_df = get_profession_df(df, key_words)
+        prof_df = self.__get_profession_df__(df, key_words)
         self.prefix = prefix
 
         self.med_salary = self.__med_salary__(df)
@@ -32,16 +14,35 @@ class DemandAnalitic():
 
         self.joined = self.join_all()
 
+    
+    def __get_analitic__(self, df, agg_func):
+        return df.groupby("year").agg({"salary": agg_func}) 
+
+
+    def __get_profession_df__(self, df, key_words):
+        first = True
+        for word in key_words:
+            if first:
+                first = False
+                mask = df["name"].str.contains(word)
+            else:
+                new_mask = df["name"].str.contains(word)
+                mask = mask | new_mask
+
+        return df.loc[mask]
+
+
     def __med_salary__(self, df, prefix=False):
-        med_salary = get_analitic(df, "median")
+        med_salary = self._get_analitic__(df, "median")
         if prefix:
             new_name = f'med_salary_{self.prefix}'
         else:
             new_name = 'med_salary'
         return med_salary.rename(columns={'salary': new_name})
 
+
     def __vac_count__ (self, df, prefix=False):
-        vac_count = get_analitic(df, "count")
+        vac_count = self.__get_analitic__(df, "count")
         if prefix:
             new_name = f'vac_count_{self.prefix}'
         else:
@@ -84,24 +85,3 @@ class DemandAnalitic():
 
     def move_to_sql(self, con, table_name):
         self.joined.to_sql(con=con, name=table_name, if_exists="append")
-
-    
-
-
-df = pd.read_csv('src/csv/converted_salary.csv')
-df['year'] = df['published_at'].str[:4]
-df = df.drop(columns=['published_at'])
-
-key_words = ["python", "питон", "пайтон"]
-
-demand = DemandAnalitic(df, key_words)
-
-# connection = sqlite3.connect('db.sqlite3')
-# cur = connection.cursor()
-
-# demand.move_to_sql(connection, 'myapp_demand')
-
-demand.save_graphs('myapp/static/graphs/demand/')
-
-
-# print("Moved to SQL")
