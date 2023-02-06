a6c4ce85bfdeccea3407d0ea13305b49d4db1523
reffy33 <reffymin@gmail.com>
2023-02-06 20:11:09 +0500

    DemandAnalitic class finished

--- src/main_refactor.py
+++ src/main_refactor.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import sqlite3
 
 def get_analitic(df, agg_func):
-        return df.groupby("year").agg({"salary": agg_func})
+    return df.groupby("year").agg({"salary": agg_func})
 
 
 def get_profession_df(df, key_words):
@@ -48,8 +48,39 @@ class DemandAnalitic():
             new_name = 'vac_count'
         return vac_count.rename(columns={"salary": new_name})
 
+    
+    def __rename_to_rus__(self, df, dict):
+        df.index.rename('Год', inplace=True)
+        return df.rename(columns=dict)
+
+    
+    def rename_all(self):
+        self.med_salary = self.__rename_to_rus__(self.med_salary, {'med_salary': "Медианная зарплата"})
+        self.med_salary_prof = self.__rename_to_rus__(self.med_salary_prof, {f'med_salary_{self.prefix}': 'Медианная зарплата'})
+
+        self.vac_count = self.__rename_to_rus__(self.vac_count, {'vac_count': "Количество вакансий"})
+        self.vac_count_prof = self.__rename_to_rus__(self.vac_count_prof, {f'vac_count_{self.prefix}': "Количество вакансий"})
+
+
+    def save_graphs(self, path, traslate=True):
+        if traslate:
+            self.rename_all()
+        
+        self.save_graph(self.med_salary, path + 'year_analitic_salary.png')
+        self.save_graph(self.vac_count, path + 'year_analitic_count.png')
+
+        self.save_graph(self.med_salary_prof, path + f'{self.prefix}_year_analitic_salary.png')
+        self.save_graph(self.vac_count_prof, path + f'{self.prefix}_year_analitic_count.png')
+    
+
+    def save_graph(self, df, full_path):
+        df.plot()
+        plt.savefig(full_path)
+
+
     def join_all(self):
         return self.med_salary.join(self.vac_count).join(self.med_salary_prof).join(self.vac_count_prof)
+
 
     def move_to_sql(self, con, table_name):
         self.joined.to_sql(con=con, name=table_name, if_exists="append")
@@ -65,9 +96,12 @@ key_words = ["python", "питон", "пайтон"]
 
 demand = DemandAnalitic(df, key_words)
 
-connection = sqlite3.connect('db.sqlite3')
-cur = connection.cursor()
+# connection = sqlite3.connect('db.sqlite3')
+# cur = connection.cursor()
 
-demand.move_to_sql(connection, 'myapp_demand')
+# demand.move_to_sql(connection, 'myapp_demand')
 
-print("Moved to SQL")
+demand.save_graphs('myapp/static/graphs/demand/')
+
+
+# print("Moved to SQL")
